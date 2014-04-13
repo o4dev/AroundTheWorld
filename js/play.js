3a87317af17c593590ad583c047c55b8aa9222a3
Luke Southam <luke@devthe,com>
2014-04-13 06:49:08 +0100

    Added more stuff, found massive bug. Need help isaac!

--- js/play.js
+++ js/play.js
@@ -3,6 +3,28 @@ function randInt(max, min) {
     return Math.floor((Math.random() * (max - min)) + min);
 }
 
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 Object.size = function(obj) {
     var size = 0, key;
     for (key in obj) {
@@ -29,55 +51,67 @@ function getOptions(navigable, x, y, z) {
         center: new google.maps.LatLng(x, y),
         tilt:                   0
     };
-    console.log("x: " + x + ", y: " + y + ", z: " + z); // do we really need this? does it give the user too much info?
+    console.log("x: " + x + ", y: " + y + ", z: " + z); // too much info?
     return mapOptions;
 }
 
+function updateScore(correct){
+    if(correct)
+        window.score++;
+}
 
+function askQuestion() {
+    var answers = arguments;
 
-function askQuestion(Country, False1, False2) {
-    // TODO: stuff to display options (via jquery)
-    //       and get response.
+    shuffle(answers);
+    
+    for(var i=0; i < answers.length; i++)
+        $('#Op' + (i+1)).text(answers[i])
+                        .click(function(){
+                            $('.option').text("");
+                            updateScore(arguments[0] == answers[i]);
+                        });
 }
 
 function goPlay() {
-    var Country = Object.keys(places)[randInt(1, Object.size(places))];
-    var CountryVars = places[Country];
-    delete places[Country];
+    var Country = Object.keys(window.places)[randInt(1, Object.size(window.places))];
+    var CountryVars = window.places[Country];
+    delete window.places[Country];
 
-    var False1 = Object.keys(places)[randInt(1, Object.size(places))];
+    var False1 = Object.keys(window.places)[randInt(1, Object.size(window.places))];
     var False2 = False1;
 
     while(False1 == False2){
-        False2 = Object.keys(places)[randInt(1, Object.size(places))];
+        False2 = Object.keys(window.places)[randInt(1, Object.size(window.places))];
     }
 
-    map.setOptions(getOptions(true,
+    // Below is not working!!
+    /**map.setOptions(getOptions(true,
                               CountryVars[0],
                               CountryVars[1],
-                              CountryVars[2]));
+                              CountryVars[2]));**/
 
-    return askQuestion(Country, False1, False2);
+    askQuestion(Country, False1, False2);
 }
 
 function init() {
-    $.getJSON("../places.json",function(json){
+    window.score = 0;
+
+    $.getJSON('../places.json',function(json){
         window.places = json;           
-    });
 
-    window.map = new google.maps.Map(document.getElementById("map"), // TODO: use jquery here?
-                                     getOptions(true, 0, 0, 0));
-
-    alert("Are you ready?");
-
-    // TODO: Get timer
-    var thereIsTime = true;
-    while(thereIsTime){
+        window.map = new google.maps.Map($('#map')[0],
+                                         getOptions(true, 0, 0, 0));
+  
+        alert("Are you ready?");
+  
+        // TODO: Get timer
         goPlay()
-    }
 
+
+        alert('Score: ' + window.score);
     });
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+ google.maps.event.addDomListener(window, 'load', init);
 
